c2af13464f38a5bbb76b44bf7d4736071ffc90e3
Moe Hein Aung <68363407+moehein-92@users.noreply.github.com>
2020-07-18 14:26:17 +0630

    Add files via upload

---
+++ to_dictionaries.py
@@ -0,0 +1,50 @@
+class Nation:
+    def __init__(self, short_name, long_name, iso_code, iso_short, iso_long, capital):
+        self.short_name = short_name
+        self.long_name = long_name
+        self.iso_code = iso_code
+        self.iso_short = iso_short
+        self.iso_long = iso_long
+        self.capital = capital
+
+#Write a function called to_dictionaries that will take as
+#input a list of instances of this class. It should return a
+#dictionary of dictionaries. The keys for the dictionaries
+#should be the short names of the nations. The values should
+#be additional dictionaries, each with five keys: long_name,
+#iso_code, iso_short, iso_long, and capital.
+#
+#For example, if we created two instances of Nation like this:
+#new_nation_1 = Nation("Albania", "Republic of Albania", 8, "AL", "ALB", "Tirana")
+#new_nation_2 = Nation("Angola", "Republic of Angola", 24, "AO", "AGO", "Luanda")
+#
+#...then made them into a list like this:
+#nation_list = [new_nation_1, new_nation_2]
+#
+#...then called the function:
+#new_dict = to_dictionaries(nation_list)
+#
+#...then we would get this dictionary in return:
+#{"Albania": {"long_name": "Republic of Albania", "iso_code": 8, "iso_short": "AL", "iso_long": "ALB", "capital": "Tirana"},
+# "Angola": {"long_name": "Republic of Angola", "iso_code": 24, "iso_short": "AO", "iso_long": "AGO", "capital": "Luanda"}}
+#
+
+def to_dictionaries(lst):
+    dict = {}
+    for i in lst:
+        dict[i.short_name] = {}
+        dict[i.short_name]["long_name"] = i.long_name
+        dict[i.short_name]["iso_code"] = i.iso_code
+        dict[i.short_name]["iso_short"] = i.iso_short
+        dict[i.short_name]["iso_long"] = i.iso_long
+        dict[i.short_name]["capital"] = i.capital
+
+    return dict
+
+#If your function works correctly, this will originally
+#print (although the order of the keys may vary) the
+#sample dictionaries shown in the directions.
+new_nation_1 = Nation("Albania", "Republic of Albania", 8, "AL", "ALB", "Tirana")
+new_nation_2 = Nation("Angola", "Republic of Angola", 24, "AO", "AGO", "Luanda")
+nation_list = [new_nation_1, new_nation_2]
+print(to_dictionaries(nation_list))
